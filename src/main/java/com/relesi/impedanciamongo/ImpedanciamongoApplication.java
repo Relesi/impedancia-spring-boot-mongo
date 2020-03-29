bdc7811553406c6ac5f4ea47c99220aafa328256
x212498 <renatolessa.2011@hotmail.com>
2020-03-28 22:29:03 -0300

    first commit impedancia spring boot with mongo

---
+++ src/main/java/com/relesi/impedanciamongo/ImpedanciamongoApplication.java
@@ -0,0 +1,13 @@
+package com.relesi.impedanciamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpedanciamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpedanciamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relesi/impedanciamongo/ImpedanciamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relesi.impedanciamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpedanciamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
